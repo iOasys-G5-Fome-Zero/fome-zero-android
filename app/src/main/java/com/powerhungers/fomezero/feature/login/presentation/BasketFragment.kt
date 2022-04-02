91498656c4f8364f5cb94dcc2cae3ad1517b4b6f
DESKTOP-VV0J4CP\rickc <hcsantos.cmp@gmail.com>
2022-04-01 21:49:33 -0300

    Basket fragment created.

---
+++ app/src/main/java/com/powerhungers/fomezero/feature/login/presentation/BasketFragment.kt
@@ -0,0 +1,24 @@
+package com.powerhungers.fomezero.feature.login.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.powerhungers.fomezero.R
+
+class BasketFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_basket, container, false)
+    }
+
+}
