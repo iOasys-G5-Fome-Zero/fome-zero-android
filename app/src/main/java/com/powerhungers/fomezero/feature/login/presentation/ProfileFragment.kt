8a4ac84e7fd14fbe40de61dff2712a6a6e6479a7
Lucas Fagundes <lukasfcarneiro@gmail.com>
2022-04-04 21:46:54 -0300

    profile layout created

--- app/src/main/java/com/powerhungers/fomezero/feature/login/presentation/ProfileFragment.kt
+++ app/src/main/java/com/powerhungers/fomezero/feature/login/presentation/ProfileFragment.kt
@@ -5,20 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.powerhungers.fomezero.R
+import com.powerhungers.fomezero.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private val binding by lazy { FragmentProfileBinding.inflate(layoutInflater) }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+    ): View {
+        return binding.root
     }
 
 }
