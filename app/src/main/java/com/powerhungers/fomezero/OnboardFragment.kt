a767907493b64e7e93e1a20c23cf2bdfdcba358c
Lucas Fagundes <lukasfcarneiro@gmail.com>
2022-04-05 21:57:29 -0300

    class Onboarding repaired

--- app/src/main/java/com/powerhungers/fomezero/OnboardFragment.kt
+++ app/src/main/java/com/powerhungers/fomezero/OnboardFragment.kt
@@ -5,18 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.powerhungers.fomezero.databinding.FragmentOnboardBinding
 
 class OnboardFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private val binding by lazy { FragmentOnboardBinding.inflate(layoutInflater) }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_onboard, container, false)
+    ): View {
+        return binding.root
     }
 
 }
