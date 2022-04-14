43308b87b957c2a0dd485c227782890e52a2af1d
Lucas Fagundes <lukasfcarneiro@gmail.com>
2022-04-14 16:35:36 -0300

    donation layout started

---
+++ app/src/main/java/com/powerhungers/fomezero/feature/donation/DonationFragment.kt
@@ -0,0 +1,20 @@
+package com.powerhungers.fomezero.feature.donation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.powerhungers.fomezero.databinding.FragmentDonationBinding
+
+class DonationFragment : Fragment() {
+
+    private val binding by lazy { FragmentDonationBinding.inflate(layoutInflater) }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return binding.root
+    }
+}
--- app/src/main/java/com/powerhungers/fomezero/feature/standbyClass/OnboardingFragment.kt
+++ app/src/main/java/com/powerhungers/fomezero/feature/standbyClass/OnboardingFragment.kt
@@ -1,4 +1,4 @@
-package com.powerhungers.fomezero
+package com.powerhungers.fomezero.feature.standbyClass
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
