2e17881f9b42c989597dc4478936eb9e0a3ca21c
DESKTOP-VV0J4CP\rickc <hcsantos.cmp@gmail.com>
2022-04-15 12:43:32 -0300

    Producer Home Fragment adjustments.

--- app/src/main/java/com/powerhungers/fomezero/feature/producer/presentation/ProducerHomeFragment.kt
+++ app/src/main/java/com/powerhungers/fomezero/feature/producer/presentation/ProducerHomeFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.powerhungers.fomezero.databinding.FragmentProducerHomeBinding
-import com.powerhungers.fomezero.feature.standbyClass.ProducerHomeFragmentDirections
 
 class ProducerHomeFragment : Fragment() {
 
@@ -40,6 +39,11 @@ class ProducerHomeFragment : Fragment() {
             btnPixCadastre.setOnClickListener {
                 findNavController().navigate(
                     ProducerHomeFragmentDirections.navigateToPaymentFragment()
+                )
+            }
+            btnSeeMessages.setOnClickListener {
+                findNavController().navigate(
+                    ProducerHomeFragmentDirections.navigateToMessagesFragment()
                 )
             }
         }
--- app/src/main/java/com/powerhungers/fomezero/feature/standbyClass/MessagesFragment.kt
+++ app/src/main/java/com/powerhungers/fomezero/feature/standbyClass/MessagesFragment.kt
@@ -5,16 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.powerhungers.fomezero.databinding.FragmentProducerHomeBinding
+import com.powerhungers.fomezero.R
 
-class ProducerHomeFragment : Fragment() {
+class MessagesFragment : Fragment() {
 
-    private val binding by lazy { FragmentProducerHomeBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        return binding.root
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_messages, container, false)
     }
+
 }
