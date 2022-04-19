2680a4ddc96864fb761a24ffeb3e52da72879790
BETIM\lucas.carneiro <lukasfcarneiro@gmail.com>
2022-04-19 16:30:23 -0300

    fragment layout and custom item view started

---
+++ app/src/main/java/com/powerhungers/fomezero/feature/consumer/signature/presentation/PlanSignatureFragment.kt
@@ -0,0 +1,24 @@
+package com.powerhungers.fomezero.feature.consumer.signature.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.powerhungers.fomezero.databinding.FragmentPlanSignatureBinding
+
+class PlanSignatureFragment : Fragment() {
+    private val binding by lazy { FragmentPlanSignatureBinding.inflate(layoutInflater) }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
