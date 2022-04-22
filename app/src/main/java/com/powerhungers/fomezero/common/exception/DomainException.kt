3ffccc9d8dd85f9d1b9b8610fa326b5520f9a4a7
DESKTOP-VV0J4CP\rickc <hcsantos.cmp@gmail.com>
2022-04-22 10:50:34 -0300

    Basket fixes.

--- app/src/main/java/com/powerhungers/fomezero/common/exception/DomainException.kt
+++ app/src/main/java/com/powerhungers/fomezero/common/exception/DomainException.kt
@@ -4,6 +4,8 @@ private const val EMPTY_EMAIL = "Campo email vazio"
 private const val EMPTY_PASSWORD = "Campo senha vazio"
 private const val EMPTY_NAME = "Campo nome vazio"
 private const val EMPTY_USER_TYPE = "Tipo do usuario não selecionado"
+private const val EMPTY_CHECKBOX = "Nenhum campo selecionado"
+
 
 open class DomainException(message: String, title: String? = null) :
     RuntimeException(message, RuntimeException(title))
@@ -15,3 +17,5 @@ class EmptyEmailException : ParamException(EMPTY_EMAIL)
 class EmptyPasswordException : ParamException(EMPTY_PASSWORD)
 class EmptyNameException : ParamException(EMPTY_NAME)
 class EmptyUserTypeException : ParamException(EMPTY_USER_TYPE)
+
+class EmptyCheckboxException : ParamException(EMPTY_CHECKBOX)
